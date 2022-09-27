3bdb0e054ccbeefa14fd9a674fbec976a40d2bc0
Anurag Dhadse <hello@adhadse.com>
2022-09-27 23:31:35 +0530

    Fix pill buttons implementation

--- assets/js/clipboard.js
+++ assets/js/clipboard.js
@@ -1,32 +1,21 @@
 import Clipboard from 'clipboard';
 
-var pre = document.getElementsByTagName('pre');
 
-for (var i = 0; i < pre.length; ++ i)
-{
-  var element = pre[i];
-  var mermaid = element.getElementsByClassName('language-mermaid')[0];
-
-  if (mermaid == null) {
-    element.insertAdjacentHTML('afterbegin', '<button id="copy">Copy</button>');
-  }
-}
-
-var clipboard = new Clipboard('#copy', {
+var clipboard = new Clipboard('.copy', {
 
   target: function(trigger) {
-    return trigger.nextElementSibling;
+    return trigger.parentElement.parentElement.getElementsByTagName("code")[0];
   },
 
 });
 
 clipboard.on('success', function(e) {
 
-    /*
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
-    */
+    
+    // console.info('Action:', e.action);
+    // console.info('Text:', e.text);
+    // console.info('Trigger:', e.trigger);
+    
 
     e.clearSelection();
 });
